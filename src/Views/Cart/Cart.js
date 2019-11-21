fd5786622cc3002cb387b572c73e7478dc9d0adb
rami <rami@holda.io>
2019-11-21 12:23:20 +0200

    fix gitignore

--- src/Views/Cart/Cart.js
+++ src/Views/Cart/Cart.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 export default function CartPage(){
     return(
     <Button variant="contained" color="primary">
-        cart
+        Cart
     </Button>
     )
 }
